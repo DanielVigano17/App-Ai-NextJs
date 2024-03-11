7926b214b7d075b94f2ff543b9535a681434b0d1
Daniel Vigano <danielvigano17@gmail.com>
2024-03-10 21:12:11 -0300

    Add clerk authentication and sidebar menu

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { IBM_Plex_Sans } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
+import { ClerkProvider } from "@clerk/nextjs";
+import { ptBR } from "@clerk/localizations";
 const IBMPlex = IBM_Plex_Sans({ 
   subsets: ["latin"],
   weight:['400','500','600','700'],
@@ -19,10 +21,16 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
+    <ClerkProvider localization={ptBR} appearance={{
+      variables:{
+        colorPrimary:"#624cf5",
+      }
+    }}>
     <html lang="en">
       <body className={cn('font-IBMPlex antialiased', IBMPlex.variable)}>
         {children}
       </body>
     </html>
+    </ClerkProvider>
   );
 }
